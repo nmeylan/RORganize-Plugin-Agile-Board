0d9fe3239abec0a6ebd6f5dab9140a5792696216
nmeylan <nmeylan@gmail.com>
2014-11-27 19:45:25 +0100

    Use some metaprogramming to reduce duplication.

--- app/decorators/user_story_decorator_link.rb
+++ app/decorators/user_story_decorator_link.rb
@@ -31,7 +31,20 @@ module UserStoryDecoratorLink
   # Build a new task link.
   def new_task_link
     h.link_to_with_permissions(h.glyph(h.t(:link_new_task), 'plus'),
-                               h.agile_board_plugin::user_story_new_task_path(context[:project].slug, model.id), context[:project], nil, {class: 'button', remote: true})
+                               h.agile_board_plugin::user_story_new_task_path(context[:project].slug, model.id),
+                               context[:project], nil, {class: 'button', remote: true})
+  end
+
+  # @param [String] caption : the caption's link.
+  # @param [Boolean] fast : do we render a fast link or not?
+  # @param [String] action : action name use to call the right method : link_to_"action".
+  # @param [Object] params : splat params.
+  def generic_link_chooser(caption, fast, action, *params)
+    if fast
+      h.send("fast_story_#{action}_link", context[:project], model.id, caption).html_safe
+    else
+      send("link_to_#{action}", *params)
+    end
   end
 
   # Render a link to show the current story. If user isn't allowed, only render the given caption.
@@ -39,18 +52,14 @@ module UserStoryDecoratorLink
   # @param [Boolean] fast : should we render the link fast or not. Fast is used when we display a huge amount of link.
   def show_link(caption, fast = false)
     if user_allowed_to?(:show)
-      choose_show_link(caption, fast)
+      generic_link_chooser(caption, fast, 'show', caption)
     else
       caption
     end
   end
 
-  def choose_show_link(caption, fast)
-    if fast
-      h.fast_story_show_link(context[:project], model.id, caption).html_safe
-    else
-      h.link_to(caption, h.agile_board_plugin::user_story_path(context[:project].slug, model.id))
-    end
+  def link_to_show(caption)
+    h.link_to(caption, h.agile_board_plugin::user_story_path(context[:project].slug, model.id))
   end
 
   # Render a link to delete the current story. If user isn't allowed, render nothing.
@@ -59,18 +68,12 @@ module UserStoryDecoratorLink
   # @param [Boolean] fast : should we render the link fast or not. Fast is used when we display a huge amount of link.
   def delete_link(button = false, path_params = {}, fast = false)
     if user_allowed_to?(:destroy)
-      choose_delete_link(button, fast, path_params)
+      generic_link_chooser( h.t(:link_delete), fast, 'delete', button, path_params)
     end
   end
 
-  def choose_delete_link(button, fast, path_params)
-    if fast
-      h.fast_story_delete_link(context[:project], model.id, h.t(:link_delete)).html_safe
-    else
-      link_to_delete(button, path_params)
-    end
-  end
-
+  # @param [Boolean] button : do we render the button style or not?
+  # @param [Hash] path_params : extra path params.
   def link_to_delete(button, path_params)
     h.link_to h.glyph(h.t(:link_delete), 'trashcan'),
               h.agile_board_plugin::user_story_path(context[:project].slug, model.id, path_params),
@@ -84,18 +87,12 @@ module UserStoryDecoratorLink
   # @param [Boolean] fast : should we render the link fast or not. Fast is used when we display a huge amount of link.
   def edit_link(button = false, path_params = {}, fast = false)
     if user_allowed_to?(:edit)
-      choose_edit_link(button, fast, path_params)
+      generic_link_chooser( h.t(:link_edit), fast, 'edit', button, path_params)
     end
   end
 
-  def choose_edit_link(button, fast, path_params)
-    if fast
-      h.fast_story_edit_link(context[:project], model.id, h.t(:link_edit)).html_safe
-    else
-      link_to_edit(button, path_params)
-    end
-  end
-
+  # @param [Boolean] button : do we render the button style or not?
+  # @param [Hash] path_params : extra path params.
   def link_to_edit(button, path_params)
     h.link_to(h.glyph(h.t(:link_edit), 'pencil'),
               h.agile_board_plugin::edit_user_story_path(context[:project].slug, model.id, path_params),
