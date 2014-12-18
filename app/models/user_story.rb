931573bd15138278ba327d6ba808cc2a0e48c205
nmeylan <nmeylan@gmail.com>
2014-12-18 18:00:08 +0100

    Reduce complexity from user story. Move change position logic into a dedicated module.

--- app/models/user_story.rb
+++ app/models/user_story.rb
@@ -4,6 +4,7 @@ class UserStory < ActiveRecord::Base
   include Rorganize::Models::Commentable
   include Rorganize::Models::Notifiable
   include Rorganize::Models::Watchable
+  include AgileBoard::Models::StoryChangePositionLogic
 
   exclude_attributes_from_journal(:description)
 
@@ -102,73 +103,7 @@ class UserStory < ActiveRecord::Base
     end
   end
 
-  def update_position
-    board = self.board
-    count_stories = board.user_stories.where(sprint_id: self.sprint_id).count
-    self.position = count_stories + 1
-  end
 
-  # Set the position of the current user story between the given previous and next stories.
-  # @param [String] prev_id : previous user story id.
-  # @param [String] next_id : next user story id.
-  def change_position(prev_id, next_id)
-    old_position = self.position
-    prev_or_next_story = prev_id ? UserStory.find_by_id(prev_id) : UserStory.find_by_id(next_id)
-    if self.sprint_id_changed? # When the sprint changed we should apply a different reorder strategy.
-      change_position_on_sprint_change(old_position, prev_id, prev_or_next_story)
-    else
-      change_position_on_reorder(old_position, prev_id, prev_or_next_story)
-    end
-  end
-
-  # @param [Numeric] old_position : the story position before we reorder it.
-  # @param [String] prev_id :  previous user story id.
-  # @param [Object] prev_or_next_story : the new neighbor for the current story.
-  # In most cases it is the previous story, but when we put the current story on the
-  # top of the list, previous is nil so we load the next.
-  def change_position_on_reorder(old_position, prev_id, prev_or_next_story)
-    if prev_or_next_story #Can be nil when the list is empty. Happening whe we reorder from the story map.
-      self.position = prev_or_next_story.position
-      if prev_or_next_story.position > old_position
-        # Decrement position for all stories that were between the old and the new position.
-        UserStory
-        .where('position > ? AND position <= ? AND id <> ?', old_position, self.position, self.id)
-        .where(sprint_id: self.sprint_id, board_id: self.board_id).update_all('position = position - 1')
-      else
-        self.position += 1 unless prev_id.nil?
-        # Increment position for all stories that were between the old and the new position.
-        UserStory
-        .where('position >= ? AND position < ? AND id <> ?', self.position, old_position, self.id)
-        .where(sprint_id: self.sprint_id, board_id: self.board_id).update_all('position = position + 1')
-      end
-    end
-  end
-
-  # @param [Numeric] old_position : the story position before we reorder it.
-  # @param [String] prev_id :  previous user story id.
-  # @param [Object] prev_or_next_story : the new neighbor for the current story.
-  # In most cases it is the previous story, but when we put the current story on the
-  # top of the list, previous is nil so we load the next.
-  def change_position_on_sprint_change(old_position, prev_id, prev_or_next_story)
-    old_sprint_id = self.sprint_id_change.first # get the old sprint id.
-    if prev_or_next_story #Can be nil when the list is empty.
-      self.position = prev_id ? prev_or_next_story.position + 1 : prev_or_next_story.position
-      # Increment position for all next stories in the new sprint.
-      UserStory
-      .where('position >= ? AND id <> ?', self.position, self.id)
-      .where(sprint_id: self.sprint_id, board_id: self.board_id).update_all('position = position + 1')
-    else
-      self.position = 1
-    end
-    # Decrement position for old sprint's stories
-    UserStory.where('position > ? AND id <> ?', old_position, self.id)
-    .where(sprint_id: old_sprint_id, board_id: self.board_id).update_all('position = position - 1')
-  end
-
-  def dec_position_on_destroy
-    position = self.position
-    UserStory.where("position > ?", position).where(sprint_id: self.sprint_id, board_id: self.board_id).update_all('position = position - 1')
-  end
 
   def detach_tasks(ids)
     issues_to_remove = self.issues.collect { |issue| issue if ids.include?(issue.id.to_s) }.compact
