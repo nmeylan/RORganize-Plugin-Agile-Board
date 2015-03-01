8ccf8d6cb52912f988b25a882709b28522cafb7e
nmeylan <nmeylan@gmail.com>
2015-03-01 17:07:43 +0100

    Fix plugin initialization phase, with the new plugin manager

--- init.rb
+++ init.rb
@@ -7,9 +7,11 @@ require 'rorganize'
 
 Rorganize::Managers::PluginManager.register :agile_board do |plugin|
   plugin.author = 'Nicolas Meylan'
-  plugin.menu(:project_menu, :agile_board, 'Agile',
+  menu_item = plugin.menu(:project_menu, :agile_board, 'Agile',
               {controller: 'boards', action: 'index'},
               {id: 'menu-boards', after: 'roadmaps', glyph: 'scrum'})
+
+  plugin.add_to_panel(:project, menu_item)
 
   plugin.add_to_always_enabled_modules([
                                            {controller: 'story_points', action: 'index'},
